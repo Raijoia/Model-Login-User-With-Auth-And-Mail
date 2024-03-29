0de41adae7264cc2033bdb5539d68f5f77a6c717
Raí Joia <megac154@gmail.com>
2024-03-29 14:18:05 -0300

    feat: add route /register/login with auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('login')
+  async login (@Body() user: { email: string; password: string }) {
+    // eslint-disable-next-line prefer-const
+    let { email, password } = user;
+
+    const userLogin = await this.userService.login({ email });
+    
+    if (!userLogin) {
+      return { message: 'User not found' };
+    }
+
+    if (userLogin) {
+      const isPasswordMatch = await this.userService.comparetor(password, userLogin.password);
+
+      if (isPasswordMatch) {
+        return userLogin;
+      } else {
+        return { message: 'Password is incorrect' };
+      }
+    }
+  }
 
   @Post('register')
   async registerUser(
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,19 +9,25 @@ const bcrypt = require('bcrypt');
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async login(data: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.findUnique({
+      where: data,
+    });
+  }
+
   async encryptPassword(password: string): Promise<string> {
     const saltRounds = process.env.SALT_ROUNDS;
 
     return bcrypt.hash(password, parseInt(saltRounds));
   }
 
-  async comparetor(password:string, hash: string): Promise<boolean> {
+  async comparetor(password: string, hash: string): Promise<boolean> {
     return bcrypt.compare(password, hash);
-  }
-
-  async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
-      data,
-    });
   }
 }
