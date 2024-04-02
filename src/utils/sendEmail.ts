43c66509fff2da3f0d716a8297335ed20e614016
Raí Joia <megac154@gmail.com>
2024-04-02 09:35:26 -0300

    fix: add environment variable for enterprise name and email for contact

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -13,7 +13,7 @@ export function sendEmail(email: string, username: string): void {
     from: process.env.MAIL,
     to: email,
     subject: `Welcome to our platform, ${username}!`,
-    html: `<h1>API Login</h1> <p>Welcome to our platform ${username}, we are very happy to have you with us.</p> </br> <p>If you were not the one who registered on our website, please contact us by email at raijoiamv@gmail.com</p>`,
+    html: `<h1>${process.env.ENTERPRISE_NAME}</h1> <p>Welcome to our platform ${username}, we are very happy to have you with us.</p> </br> <p>If you were not the one who registered on our website, please contact us by email at ${process.env.EMAIL_CONTACT}</p>`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
